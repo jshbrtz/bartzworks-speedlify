5a84e21c9d041ad42ba9bb2b5d915a5238c372cf
jshbrtz <jsh@brtz.me>
2026-02-11 13:36:18 -0600

    Update bartzworks.js

--- _data/sites/bartzworks.js
+++ _data/sites/bartzworks.js
@@ -1,9 +1,6 @@
 module.exports = {
 	name: "bartz.works", // optional, falls back to object key
 	description: "How well am I doing at web development?",
-	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
 		frequency: 60 * 168, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
