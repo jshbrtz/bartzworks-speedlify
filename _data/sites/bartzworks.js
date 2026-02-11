c71636e6777c66743522c263bf9fdcd3cfecc2de
jshbrtz <jsh@brtz.me>
2026-02-11 13:23:35 -0600

    Prepping for launch

--- _data/sites/bartzworks.js
+++ _data/sites/bartzworks.js
@@ -1,26 +1,24 @@
 module.exports = {
-	description: "Front-end Testing Tools",
+	name: "bartz.works", // optional, falls back to object key
+	description: "How well am I doing at web development?",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 11 + 30, // 11h, 30m
+		frequency: 60 * 168, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
 		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site",
+		freshChrome: "site"
 	},
 	urls: [
-		"https://eslint.org/",
-		"https://qunitjs.com/",
-		"https://karma-runner.github.io/latest/index.html",
-		"https://gulpjs.com/",
-		"https://webhint.io/",
-		"https://gruntjs.com/",
-		"https://theintern.io/",
-		"https://istanbul.js.org/",
-		"https://webdriver.io/",
-		"https://mochajs.org/"
+		"https://bartz.works",
+		"https://bartz.works/garden/",
+		"https://bartz.works/library/",
+		"https://bartz.works/now/",
+		"https://bartz.works/about/",
+		// Popular Posts
+		"https://bartz.works/prompting-readers-to-rotate-their-phones/"
 	]
 };
